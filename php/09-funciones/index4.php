3ae10e2511fdca4829c929e23121138abd7af4ee
Jose Palencia <joc.palencia@gmail.com>
2020-07-07 15:42:37 -0600

    Funciones

---
+++ php/09-funciones/index4.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	function suma($numero1, $numero2){
+
+		return $numero1 + $numero2;
+
+	}
+
+	echo "Suma: " . suma(2, 2);
+
+
+	// function conectar(){
+		
+	// 		..........
+	// 		.........
+	// 		......
+		
+
+	// 	return false;
+	// }
+
+?>
